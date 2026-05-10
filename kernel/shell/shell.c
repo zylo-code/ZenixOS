b924dd16625fec045d2d7312a744461caa35e9f3
zylo-code <profidzon914@gmail.com>
2026-05-10 16:48:49 +0300

    I updated the shell and added two commands: help and clear screen.

--- kernel/shell/shell.c
+++ kernel/shell/shell.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include "../system/memory.h"
-#include "../drivers/terminal/terminal.h"
+#include "terminal/terminal.h"
 
 char shell_buffer[64] = {0};
 int shell_idx = 0;
@@ -19,8 +19,16 @@ void shell_handler_char(char c) {
         shell_buffer[shell_idx] = '\0';
 
         if (strcmp(shell_buffer, "help") == 0) {
-            terminal_writeln("help");
+            terminal_writeln("");
+            terminal_writeln("Commands:");
+            terminal_writeln("clear - clears the screen");
+        } else if (strcmp(shell_buffer, "clear") == 0) {
+            terminal_clear();
+            terminal_writeln("");
+            terminal_write("user@zenixos:~$ ");
+        shell_buffer[shell_idx] = '\0';
         } else if (shell_buffer[0] != '\0') {
+            terminal_writeln("");
             terminal_writeln("Unknow command.");
         }
 
