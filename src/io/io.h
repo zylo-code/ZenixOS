462fa8b990c1685dbe7c40b0dcc61e06a9a0ee4c
zylo-code <profidzon914@gmail.com>
2026-05-10 16:36:32 +0300

    Corrected the project structure and added Makefile

--- src/io/io.h
+++
@@ -1,7 +0,0 @@
-#ifndef IO_H
-#define IO_H
-
-unsigned char inb(unsigned short port);
-void outb(unsigned short port, unsigned char value);
-
-#endif
--- src/shell/shell.h
+++
@@ -1,7 +0,0 @@
-#ifndef SHELL_H
-#define SHELL_H
-
-void shell_init(void);
-void shell_handler_char(char c);
-
-#endif
